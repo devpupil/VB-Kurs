6204122eacd0e64556112461dca1d34925d1264b
Dev Pupil <devpubil@gmail.com>
2013-04-10 17:06:29 +0200

    Projekt mit NugetInstallation, geht schief wegen der Verweise, alte DLL eingefügt

---
+++ NeuerSQLiteTEST1/NeuerSQLiteTEST1/frmSQLiteTEST.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSQLiteTEST
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_createdb = New System.Windows.Forms.Button()
+        Me.btn_createtable = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn_createdb
+        '
+        Me.btn_createdb.Location = New System.Drawing.Point(18, 17)
+        Me.btn_createdb.Name = "btn_createdb"
+        Me.btn_createdb.Size = New System.Drawing.Size(187, 61)
+        Me.btn_createdb.TabIndex = 0
+        Me.btn_createdb.Text = "DB erstellen"
+        Me.btn_createdb.UseVisualStyleBackColor = True
+        '
+        'btn_createtable
+        '
+        Me.btn_createtable.Location = New System.Drawing.Point(13, 100)
+        Me.btn_createtable.Name = "btn_createtable"
+        Me.btn_createtable.Size = New System.Drawing.Size(191, 74)
+        Me.btn_createtable.TabIndex = 1
+        Me.btn_createtable.Text = "Tabellen erstellen"
+        Me.btn_createtable.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(13, 191)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(192, 76)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Tabelle füllen"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'frmSQLiteTEST
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(435, 369)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btn_createtable)
+        Me.Controls.Add(Me.btn_createdb)
+        Me.Name = "frmSQLiteTEST"
+        Me.Text = "SQLite TEST"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btn_createdb As System.Windows.Forms.Button
+    Friend WithEvents btn_createtable As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ NeuerSQLiteTEST1/NeuerSQLiteTEST1/frmSQLiteTEST.vb
@@ -0,0 +1,85 @@
+﻿Public Class frmSQLiteTEST
+
+    Private Sub btn_createdb_Click(sender As Object, e As EventArgs) Handles btn_createdb.Click
+        ' Öffnet Datei speichern
+        Dim f As New SaveFileDialog
+        f.Filter = "SQLite 3 (*.db3) |*.db3|All Files|"
+
+        If f.ShowDialog = Windows.Forms.DialogResult.OK Then
+
+            ' Datenbankdatei erstellen
+            Dim SQLconnect As New SQLite.SQLiteConnection()
+
+            ' Die Datenbank existiert noch nicht, Wir erstellen den Pfad
+            SQLconnect.ConnectionString = "Data Source=" & f.FileName & ";"
+            SQLconnect.Open()
+            SQLconnect.Close()
+
+        Else
+            MsgBox("Abbrechen gedrückt")
+        End If
+    End Sub
+
+    Private Sub btn_createtable_Click(sender As Object, e As EventArgs) Handles btn_createtable.Click
+        Dim f As New OpenFileDialog
+        f.Filter = "SQLite 3 (*.db3) |*.db3|All Files|*.*"
+
+        If f.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim SQLconnect As New SQLite.SQLiteConnection
+            Dim SQLcommand As SQLite.SQLiteCommand
+            SQLconnect.ConnectionString = "Data Source=" & f.FileName & ";"
+            SQLconnect.Open()
+
+            SQLcommand = SQLconnect.CreateCommand
+
+            ' SQL Query um die Tabellen anzulegen
+            SQLcommand.CommandText = "CREATE TABLE tbl_adressen(id INTEGER PRIMARY KEY AUTOINCREMENT, vorname TEXT, nachname TEXT, strasse TEXT, plz TEXT, ort TEXT, bild BLOB);"
+            SQLcommand.ExecuteNonQuery()
+            SQLcommand.Dispose()
+            SQLconnect.Close()
+        Else
+            MsgBox("Abrechen gedrückt")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim f As New OpenFileDialog
+        Dim zaehler As Integer
+        f.Filter = "SQLite 3 (*.db3) |*.db3|All Files|*.*"
+
+        If f.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim SQLconnect As New SQLite.SQLiteConnection
+            Dim SQLcommand As SQLite.SQLiteCommand
+            SQLconnect.ConnectionString = "Data Source=" & f.FileName & ";"
+            SQLconnect.Open()
+
+            SQLcommand = SQLconnect.CreateCommand
+
+            ' Zaehler einlesen um die Datenbank zu füllen
+            frmDBFuel.txt_datasets.Focus()
+            frmDBFuel.txt_datasets.SelectAll()
+            frmDBFuel.ShowDialog()
+
+            If frmDBFuel.DialogResult = Windows.Forms.DialogResult.OK Then
+
+                zaehler = Int(frmDBFuel.txt_datasets.Text)
+                ' SQL Query um die Tabellen mit einer bestimmten Anzahl an Datensätzen zu füllen
+
+                For i As Integer = 0 To zaehler
+                    ' SQL Query String
+                    Dim sqlquery As String = "INSERT INTO tbl_adressen (vorname, nachname, strasse, plz, ort) VALUES('Max" & i & "','Muster" & i & "','Strasse" & i & "','11111','Musterstadt" & i & "');"
+
+                    SQLcommand.CommandText = sqlquery
+                    SQLcommand.ExecuteNonQuery()
+                Next
+                MsgBox(zaehler & " Datensätze gespeichert")
+            Else
+                MsgBox("Keinen Zähler eingegeben")
+            End If
+            SQLcommand.Dispose()
+            SQLconnect.Close()
+        Else
+            MsgBox("Abrechen gedrückt")
+        End If
+    End Sub
+End Class
