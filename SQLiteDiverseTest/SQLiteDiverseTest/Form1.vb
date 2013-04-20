16ad85a2d272606ea960ade70e37d91652ba43f5
devpupil <devpupil@gmail.com>
2013-04-20 13:51:10 +0200

    Panel Design und Label Autosize

--- SQLiteDiverseTest/SQLiteDiverseTest/Form1.vb
+++ SQLiteDiverseTest/SQLiteDiverseTest/Form1.vb
@@ -8,4 +8,15 @@
         Dialog1.Show()
 
     End Sub
+
+    Private Sub TableLayoutPanel1_Click(sender As Object, e As EventArgs) Handles TableLayoutPanel1.Click
+        Dialog1.TopLevel = False
+        Dialog1.TextBox1.Text = "Label gedrückt Click"
+        Me.Panel1.Controls.Add(Dialog1)
+        Dialog1.Show()
+    End Sub
+
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles TableLayoutPanel1.Paint
+
+    End Sub
 End Class
