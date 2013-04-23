96c749dc5a579cacdc2feb709de05b71b7b707cd
Dev Pupil <devpubil@gmail.com>
2013-04-23 17:04:41 +0200

    Übung wie Original fertiggemacht Versuch zurückgestellte Fragen wieder einzubinden
    Signed-off-by: Dev Pupil <devpubil@gmail.com>

--- lernkartei2/Lernkartei2/Lernkartei2/frmLernen.Designer.vb
+++ lernkartei2/Lernkartei2/Lernkartei2/frmLernen.Designer.vb
@@ -31,6 +31,7 @@ Partial Class frmLernen
         Dim AntwortFreiLabel As System.Windows.Forms.Label
         Dim PunkteLabel As System.Windows.Forms.Label
         Dim ThemenlinkLabel As System.Windows.Forms.Label
+        Dim ZurueckstellenLabel As System.Windows.Forms.Label
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.Mc_anwort1TextBox = New System.Windows.Forms.TextBox()
         Me.Mc_check1CheckBox = New System.Windows.Forms.CheckBox()
@@ -47,6 +48,14 @@ Partial Class frmLernen
         Me.PunkteTextBox = New System.Windows.Forms.TextBox()
         Me.ThemenlinkTextBox = New System.Windows.Forms.TextBox()
         Me.txtID = New System.Windows.Forms.TextBox()
+        Me.ZurueckstellenCheckBox = New System.Windows.Forms.CheckBox()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TableLayoutPanel3 = New System.Windows.Forms.TableLayoutPanel()
+        Me.btnErster = New System.Windows.Forms.Button()
+        Me.btnZurück = New System.Windows.Forms.Button()
+        Me.btnNächster = New System.Windows.Forms.Button()
+        Me.btnLetzter = New System.Windows.Forms.Button()
+        Me.btnRueckNochmal = New System.Windows.Forms.Button()
         Mc_anwort1Label = New System.Windows.Forms.Label()
         Mc_anwort2Label = New System.Windows.Forms.Label()
         Mc_anwort3Label = New System.Windows.Forms.Label()
@@ -56,41 +65,11 @@ Partial Class frmLernen
         AntwortFreiLabel = New System.Windows.Forms.Label()
         PunkteLabel = New System.Windows.Forms.Label()
         ThemenlinkLabel = New System.Windows.Forms.Label()
+        ZurueckstellenLabel = New System.Windows.Forms.Label()
         Me.TableLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.TableLayoutPanel3.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'TableLayoutPanel1
-        '
-        Me.TableLayoutPanel1.ColumnCount = 3
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.05547!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.49604!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7.290016!))
-        Me.TableLayoutPanel1.Controls.Add(Mc_anwort1Label, 0, 1)
-        Me.TableLayoutPanel1.Controls.Add(Me.Mc_anwort1TextBox, 1, 1)
-        Me.TableLayoutPanel1.Controls.Add(Me.Mc_check1CheckBox, 2, 1)
-        Me.TableLayoutPanel1.Controls.Add(Mc_anwort2Label, 0, 2)
-        Me.TableLayoutPanel1.Controls.Add(Mc_anwort3Label, 0, 3)
-        Me.TableLayoutPanel1.Controls.Add(Mc_anwort4Label, 0, 4)
-        Me.TableLayoutPanel1.Controls.Add(Mc_anwort5Label, 0, 5)
-        Me.TableLayoutPanel1.Controls.Add(Me.Mc_anwort2TextBox, 1, 2)
-        Me.TableLayoutPanel1.Controls.Add(Me.Mc_anwort3TextBox, 1, 3)
-        Me.TableLayoutPanel1.Controls.Add(Me.Mc_anwort5TextBox, 1, 5)
-        Me.TableLayoutPanel1.Controls.Add(Me.Mc_anwort4TextBox, 1, 4)
-        Me.TableLayoutPanel1.Controls.Add(Me.Mc_check2CheckBox, 2, 2)
-        Me.TableLayoutPanel1.Controls.Add(Me.Mc_check3CheckBox, 2, 3)
-        Me.TableLayoutPanel1.Controls.Add(Me.Mc_check4CheckBox, 2, 4)
-        Me.TableLayoutPanel1.Controls.Add(Me.Mc_check5CheckBox, 2, 5)
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(17, 184)
-        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 6
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(631, 238)
-        Me.TableLayoutPanel1.TabIndex = 56
         '
         'Mc_anwort1Label
         '
@@ -100,22 +79,6 @@ Partial Class frmLernen
         Mc_anwort1Label.Size = New System.Drawing.Size(65, 13)
         Mc_anwort1Label.TabIndex = 11
         Mc_anwort1Label.Text = "mc anwort1:"
-        '
-        'Mc_anwort1TextBox
-        '
-        Me.Mc_anwort1TextBox.Location = New System.Drawing.Point(98, 42)
-        Me.Mc_anwort1TextBox.Multiline = True
-        Me.Mc_anwort1TextBox.Name = "Mc_anwort1TextBox"
-        Me.Mc_anwort1TextBox.Size = New System.Drawing.Size(483, 33)
-        Me.Mc_anwort1TextBox.TabIndex = 12
-        '
-        'Mc_check1CheckBox
-        '
-        Me.Mc_check1CheckBox.Location = New System.Drawing.Point(587, 42)
-        Me.Mc_check1CheckBox.Name = "Mc_check1CheckBox"
-        Me.Mc_check1CheckBox.Size = New System.Drawing.Size(41, 24)
-        Me.Mc_check1CheckBox.TabIndex = 22
-        Me.Mc_check1CheckBox.UseVisualStyleBackColor = True
         '
         'Mc_anwort2Label
         '
@@ -152,6 +115,91 @@ Partial Class frmLernen
         Mc_anwort5Label.Size = New System.Drawing.Size(65, 13)
         Mc_anwort5Label.TabIndex = 19
         Mc_anwort5Label.Text = "mc anwort5:"
+        '
+        'FrageLabel
+        '
+        FrageLabel.AutoSize = True
+        FrageLabel.Location = New System.Drawing.Point(14, 12)
+        FrageLabel.Name = "FrageLabel"
+        FrageLabel.Size = New System.Drawing.Size(37, 13)
+        FrageLabel.TabIndex = 48
+        FrageLabel.Text = "Frage:"
+        '
+        'AntwortFreiLabel
+        '
+        AntwortFreiLabel.AutoSize = True
+        AntwortFreiLabel.Location = New System.Drawing.Point(14, 76)
+        AntwortFreiLabel.Name = "AntwortFreiLabel"
+        AntwortFreiLabel.Size = New System.Drawing.Size(65, 13)
+        AntwortFreiLabel.TabIndex = 50
+        AntwortFreiLabel.Text = "antwort Frei:"
+        '
+        'PunkteLabel
+        '
+        PunkteLabel.AutoSize = True
+        PunkteLabel.Location = New System.Drawing.Point(14, 431)
+        PunkteLabel.Name = "PunkteLabel"
+        PunkteLabel.Size = New System.Drawing.Size(43, 13)
+        PunkteLabel.TabIndex = 52
+        PunkteLabel.Text = "punkte:"
+        '
+        'ThemenlinkLabel
+        '
+        ThemenlinkLabel.AutoSize = True
+        ThemenlinkLabel.Location = New System.Drawing.Point(14, 460)
+        ThemenlinkLabel.Name = "ThemenlinkLabel"
+        ThemenlinkLabel.Size = New System.Drawing.Size(65, 13)
+        ThemenlinkLabel.TabIndex = 54
+        ThemenlinkLabel.Text = "Themenlink:"
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 3
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.05547!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.49604!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7.290016!))
+        Me.TableLayoutPanel1.Controls.Add(Mc_anwort1Label, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Mc_anwort1TextBox, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Mc_check1CheckBox, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Mc_anwort2Label, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Mc_anwort3Label, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Mc_anwort4Label, 0, 4)
+        Me.TableLayoutPanel1.Controls.Add(Mc_anwort5Label, 0, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.Mc_anwort2TextBox, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Mc_anwort3TextBox, 1, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Mc_anwort5TextBox, 1, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.Mc_anwort4TextBox, 1, 4)
+        Me.TableLayoutPanel1.Controls.Add(Me.Mc_check2CheckBox, 2, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Mc_check3CheckBox, 2, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Mc_check4CheckBox, 2, 4)
+        Me.TableLayoutPanel1.Controls.Add(Me.Mc_check5CheckBox, 2, 5)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(17, 184)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 6
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(631, 238)
+        Me.TableLayoutPanel1.TabIndex = 56
+        '
+        'Mc_anwort1TextBox
+        '
+        Me.Mc_anwort1TextBox.Location = New System.Drawing.Point(98, 42)
+        Me.Mc_anwort1TextBox.Multiline = True
+        Me.Mc_anwort1TextBox.Name = "Mc_anwort1TextBox"
+        Me.Mc_anwort1TextBox.Size = New System.Drawing.Size(483, 33)
+        Me.Mc_anwort1TextBox.TabIndex = 12
+        '
+        'Mc_check1CheckBox
+        '
+        Me.Mc_check1CheckBox.Location = New System.Drawing.Point(587, 42)
+        Me.Mc_check1CheckBox.Name = "Mc_check1CheckBox"
+        Me.Mc_check1CheckBox.Size = New System.Drawing.Size(41, 24)
+        Me.Mc_check1CheckBox.TabIndex = 22
+        Me.Mc_check1CheckBox.UseVisualStyleBackColor = True
         '
         'Mc_anwort2TextBox
         '
@@ -217,48 +265,24 @@ Partial Class frmLernen
         Me.Mc_check5CheckBox.TabIndex = 30
         Me.Mc_check5CheckBox.UseVisualStyleBackColor = True
         '
-        'FrageLabel
-        '
-        FrageLabel.AutoSize = True
-        FrageLabel.Location = New System.Drawing.Point(14, 12)
-        FrageLabel.Name = "FrageLabel"
-        FrageLabel.Size = New System.Drawing.Size(37, 13)
-        FrageLabel.TabIndex = 48
-        FrageLabel.Text = "Frage:"
-        '
         'FrageTextBox
         '
         Me.FrageTextBox.Location = New System.Drawing.Point(98, 12)
         Me.FrageTextBox.Multiline = True
         Me.FrageTextBox.Name = "FrageTextBox"
+        Me.FrageTextBox.ReadOnly = True
+        Me.FrageTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
         Me.FrageTextBox.Size = New System.Drawing.Size(500, 55)
         Me.FrageTextBox.TabIndex = 49
-        '
-        'AntwortFreiLabel
-        '
-        AntwortFreiLabel.AutoSize = True
-        AntwortFreiLabel.Location = New System.Drawing.Point(14, 76)
-        AntwortFreiLabel.Name = "AntwortFreiLabel"
-        AntwortFreiLabel.Size = New System.Drawing.Size(65, 13)
-        AntwortFreiLabel.TabIndex = 50
-        AntwortFreiLabel.Text = "antwort Frei:"
         '
         'AntwortFreiTextBox
         '
         Me.AntwortFreiTextBox.Location = New System.Drawing.Point(98, 73)
         Me.AntwortFreiTextBox.Multiline = True
         Me.AntwortFreiTextBox.Name = "AntwortFreiTextBox"
+        Me.AntwortFreiTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
         Me.AntwortFreiTextBox.Size = New System.Drawing.Size(500, 105)
         Me.AntwortFreiTextBox.TabIndex = 51
-        '
-        'PunkteLabel
-        '
-        PunkteLabel.AutoSize = True
-        PunkteLabel.Location = New System.Drawing.Point(14, 431)
-        PunkteLabel.Name = "PunkteLabel"
-        PunkteLabel.Size = New System.Drawing.Size(43, 13)
-        PunkteLabel.TabIndex = 52
-        PunkteLabel.Text = "punkte:"
         '
         'PunkteTextBox
         '
@@ -266,15 +290,6 @@ Partial Class frmLernen
         Me.PunkteTextBox.Name = "PunkteTextBox"
         Me.PunkteTextBox.Size = New System.Drawing.Size(104, 20)
         Me.PunkteTextBox.TabIndex = 53
-        '
-        'ThemenlinkLabel
-        '
-        ThemenlinkLabel.AutoSize = True
-        ThemenlinkLabel.Location = New System.Drawing.Point(14, 460)
-        ThemenlinkLabel.Name = "ThemenlinkLabel"
-        ThemenlinkLabel.Size = New System.Drawing.Size(65, 13)
-        ThemenlinkLabel.TabIndex = 54
-        ThemenlinkLabel.Text = "Themenlink:"
         '
         'ThemenlinkTextBox
         '
@@ -294,11 +309,107 @@ Partial Class frmLernen
         Me.txtID.Size = New System.Drawing.Size(36, 13)
         Me.txtID.TabIndex = 57
         '
+        'ZurueckstellenLabel
+        '
+        ZurueckstellenLabel.AutoSize = True
+        ZurueckstellenLabel.Location = New System.Drawing.Point(221, 615)
+        ZurueckstellenLabel.Name = "ZurueckstellenLabel"
+        ZurueckstellenLabel.Size = New System.Drawing.Size(99, 13)
+        ZurueckstellenLabel.TabIndex = 58
+        ZurueckstellenLabel.Text = "Frage zurückstellen"
+        '
+        'ZurueckstellenCheckBox
+        '
+        Me.ZurueckstellenCheckBox.Location = New System.Drawing.Point(326, 610)
+        Me.ZurueckstellenCheckBox.Name = "ZurueckstellenCheckBox"
+        Me.ZurueckstellenCheckBox.Size = New System.Drawing.Size(23, 24)
+        Me.ZurueckstellenCheckBox.TabIndex = 59
+        Me.ZurueckstellenCheckBox.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.ZurueckstellenCheckBox.UseVisualStyleBackColor = True
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 2
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.btnErster, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.btnZurück, 1, 0)
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(17, 598)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 1
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(185, 42)
+        Me.TableLayoutPanel2.TabIndex = 60
+        '
+        'TableLayoutPanel3
+        '
+        Me.TableLayoutPanel3.ColumnCount = 2
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.Controls.Add(Me.btnLetzter, 0, 0)
+        Me.TableLayoutPanel3.Controls.Add(Me.btnNächster, 0, 0)
+        Me.TableLayoutPanel3.Location = New System.Drawing.Point(507, 598)
+        Me.TableLayoutPanel3.Name = "TableLayoutPanel3"
+        Me.TableLayoutPanel3.RowCount = 1
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel3.Size = New System.Drawing.Size(185, 42)
+        Me.TableLayoutPanel3.TabIndex = 61
+        '
+        'btnErster
+        '
+        Me.btnErster.Location = New System.Drawing.Point(3, 3)
+        Me.btnErster.Name = "btnErster"
+        Me.btnErster.Size = New System.Drawing.Size(86, 33)
+        Me.btnErster.TabIndex = 0
+        Me.btnErster.Text = "|< Erster"
+        Me.btnErster.UseVisualStyleBackColor = True
+        '
+        'btnZurück
+        '
+        Me.btnZurück.Location = New System.Drawing.Point(95, 3)
+        Me.btnZurück.Name = "btnZurück"
+        Me.btnZurück.Size = New System.Drawing.Size(86, 33)
+        Me.btnZurück.TabIndex = 1
+        Me.btnZurück.Text = "<< Zurück"
+        Me.btnZurück.UseVisualStyleBackColor = True
+        '
+        'btnNächster
+        '
+        Me.btnNächster.Location = New System.Drawing.Point(3, 3)
+        Me.btnNächster.Name = "btnNächster"
+        Me.btnNächster.Size = New System.Drawing.Size(86, 33)
+        Me.btnNächster.TabIndex = 1
+        Me.btnNächster.Text = "Nächster >>"
+        Me.btnNächster.UseVisualStyleBackColor = True
+        '
+        'btnLetzter
+        '
+        Me.btnLetzter.Location = New System.Drawing.Point(95, 3)
+        Me.btnLetzter.Name = "btnLetzter"
+        Me.btnLetzter.Size = New System.Drawing.Size(86, 33)
+        Me.btnLetzter.TabIndex = 2
+        Me.btnLetzter.Text = "Letzter >|"
+        Me.btnLetzter.UseVisualStyleBackColor = True
+        '
+        'btnRueckNochmal
+        '
+        Me.btnRueckNochmal.Location = New System.Drawing.Point(346, 602)
+        Me.btnRueckNochmal.Name = "btnRueckNochmal"
+        Me.btnRueckNochmal.Size = New System.Drawing.Size(95, 32)
+        Me.btnRueckNochmal.TabIndex = 62
+        Me.btnRueckNochmal.Text = "Anzeigen"
+        Me.btnRueckNochmal.UseVisualStyleBackColor = True
+        '
         'frmLernen
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(704, 648)
+        Me.ClientSize = New System.Drawing.Size(704, 652)
+        Me.Controls.Add(Me.btnRueckNochmal)
+        Me.Controls.Add(Me.TableLayoutPanel3)
+        Me.Controls.Add(ZurueckstellenLabel)
+        Me.Controls.Add(Me.TableLayoutPanel2)
+        Me.Controls.Add(Me.ZurueckstellenCheckBox)
         Me.Controls.Add(Me.txtID)
         Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Controls.Add(FrageLabel)
@@ -313,6 +424,8 @@ Partial Class frmLernen
         Me.Text = "frmLernen"
         Me.TableLayoutPanel1.ResumeLayout(False)
         Me.TableLayoutPanel1.PerformLayout()
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.TableLayoutPanel3.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -333,4 +446,12 @@ Partial Class frmLernen
     Friend WithEvents PunkteTextBox As System.Windows.Forms.TextBox
     Friend WithEvents ThemenlinkTextBox As System.Windows.Forms.TextBox
     Friend WithEvents txtID As System.Windows.Forms.TextBox
+    Friend WithEvents ZurueckstellenCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents TableLayoutPanel2 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents btnErster As System.Windows.Forms.Button
+    Friend WithEvents btnZurück As System.Windows.Forms.Button
+    Friend WithEvents TableLayoutPanel3 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents btnLetzter As System.Windows.Forms.Button
+    Friend WithEvents btnNächster As System.Windows.Forms.Button
+    Friend WithEvents btnRueckNochmal As System.Windows.Forms.Button
 End Class
--- lernkartei2/Lernkartei2/Lernkartei2/frmLernen.vb
+++ lernkartei2/Lernkartei2/Lernkartei2/frmLernen.vb
@@ -4,41 +4,163 @@ Public Class frmLernen
     ' private Variablen nur für diese Klasse ereichbar anlegen
     Private da As OleDbDataAdapter = Nothing
     Private dt As DataTable = Nothing
+    Private dt1 As DataTable = Nothing
     Private bs As New BindingSource
 
     Private Sub frmLernen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        '
-        ' Objektvariablen anlegen für die Datenbankverbindung, der Connectionstring wird als Parameter übergeben
-        '
-        Dim con As New OleDbConnection(My.Settings.lernkartei2ConnectionString)
+        Try
 
-        ' SQL String für die Abfrage
-        Dim sqlStr As String = "SELECT tbl_Fragen.* FROM tbl_Fragen WHERE tbl_Fragen.cat_id=" & My.Settings.KatWahl & ";"
+            '
+            ' Objektvariablen anlegen für die Datenbankverbindung, der Connectionstring wird als Parameter übergeben
+            '
+            Dim con As New OleDbConnection(My.Settings.lernkartei2ConnectionString)
 
-        ' Die Connection und der SQL String werden auf den Data Adapter geladen
-        da = New OleDbDataAdapter(sqlStr, con)
+            ' SQL String für die Abfrage
+            Dim sqlStr As String = "SELECT tbl_Fragen.* FROM tbl_Fragen WHERE tbl_Fragen.cat_id=" & My.Settings.KatWahl & ";"
 
-        ' Schemadataadapter für Spalten der Tabellen und Primärschlüssel
-        da.MissingSchemaAction = MissingSchemaAction.AddWithKey
+            ' Die Connection und der SQL String werden auf den Data Adapter geladen
+            da = New OleDbDataAdapter(sqlStr, con)
 
-        ' neue Datentabelle anelgen, der String muss nicht mit dem Tabellennamen übereinstimmen
-        dt = New DataTable("tblFragen")
+            ' Schemadataadapter für Spalten der Tabellen und Primärschlüssel
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey
 
-        ' die Verbindung zu Datenbank öffnen
-        con.Open()
+            ' neue Datentabelle anelgen, der String muss nicht mit dem Tabellennamen übereinstimmen
+            dt = New DataTable("tblFragen")
 
-        ' Die Datentabelle füllen
-        da.Fill(dt)
+            ' die Verbindung zu Datenbank öffnen
+            con.Open()
 
-        ' Die Verbindung wieder beenden
-        con.Close()
+            ' Die Datentabelle füllen
+            da.Fill(dt)
 
-        ' Die Databindingsource mit der Datenquelle belegen
-        bs.DataSource = dt
+            ' Die Verbindung wieder beenden
+            con.Close()
 
-        ' Jedes Element auf dem Formular muss mit dem Bindingsource gebunden werden
-        txtID.DataBindings.Add("text", bs, "ID")
-        FrageTextBox.DataBindings.Add("text", bs, "frage")
+            ' Die Databindingsource mit der Datenquelle belegen
+            bs.DataSource = dt
+
+            ' Jedes Element auf dem Formular muss mit dem Bindingsource gebunden werden
+            txtID.DataBindings.Add("text", bs, "ID")
+            FrageTextBox.DataBindings.Add("text", bs, "frage")
+            AntwortFreiTextBox.DataBindings.Add("text", bs, "pr_antwortFrei")
+
+            ' Multiple Choice
+            Mc_anwort1TextBox.DataBindings.Add("text", bs, "mc_anwort1")
+            Mc_check1CheckBox.DataBindings.Add("Checked", bs, "pr_mc_check1")
+
+            Mc_anwort2TextBox.DataBindings.Add("text", bs, "mc_anwort2")
+            Mc_check2CheckBox.DataBindings.Add("Checked", bs, "pr_mc_check2")
+
+            Mc_anwort3TextBox.DataBindings.Add("text", bs, "mc_anwort3")
+            Mc_check3CheckBox.DataBindings.Add("Checked", bs, "pr_mc_check3")
+
+            Mc_anwort4TextBox.DataBindings.Add("text", bs, "mc_anwort4")
+            Mc_check4CheckBox.DataBindings.Add("Checked", bs, "pr_mc_check4")
+
+            Mc_anwort5TextBox.DataBindings.Add("text", bs, "mc_anwort5")
+            Mc_check5CheckBox.DataBindings.Add("Checked", bs, "pr_mc_check5")
+
+            ' Zurückstellen der Frage
+            ZurueckstellenCheckBox.DataBindings.Add("Checked", bs, "zurueckstellen")
+
+        Catch ex As Exception
+            ' Aufruf unserer Fehlerroutine
+            If FehlerAnezige(ex) = True Then
+                MsgBox("Anwendung wird beendet")
+                Me.Close()
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub btnErster_Click(sender As Object, e As EventArgs) Handles btnErster.Click
+        ' den aktuellen Datensatz aus dem Speicher löschen
+        ' bs.RemoveCurrent()
+        ' Anwendung der Änderung in der Datenquelle
+        bs.EndEdit()
+        da.Update(dt)
+        ' Den Bindingsource auf den ersten Datensatz  stellen
+        bs.MoveFirst()
+
+    End Sub
+
+    Private Sub btnZurück_Click(sender As Object, e As EventArgs) Handles btnZurück.Click
+       
+        ' Anwendung der Änderung in der Datenquelle
+        bs.EndEdit()
+        da.Update(dt)
+
+        bs.MovePrevious()
+    End Sub
+
+    Private Sub btnNächster_Click(sender As Object, e As EventArgs) Handles btnNächster.Click
+        
+        ' Anwendung der Änderung in der Datenquelle
+        bs.EndEdit()
+        da.Update(dt)
+
+        bs.MoveNext()
+    End Sub
+
+    Private Sub btnLetzter_Click(sender As Object, e As EventArgs) Handles btnLetzter.Click
+       
+        ' Anwendung der Änderung in der Datenquelle
+        bs.EndEdit()
+        da.Update(dt)
+
+        bs.MoveLast()
+    End Sub
+
+    Private Sub btnRueckNochmal_Click(sender As Object, e As EventArgs) Handles btnRueckNochmal.Click
+
+        Try
+            ' neue Datentabelle anelgen, der String muss nicht mit dem Tabellennamen übereinstimmen
+            dt1 = New DataTable("tblFragenRueckgestellt")
+
+            ' die Verbindung zu Datenbank öffnen
+            'con.Open()
+
+            ' Die Datentabelle füllen
+            da.Fill(dt)
+
+            ' Die Verbindung wieder beenden
+            'con.Close()
+
+            ' Die Databindingsource mit der Datenquelle belegen
+            bs.DataSource = dt
+
+            ' Jedes Element auf dem Formular muss mit dem Bindingsource gebunden werden
+            txtID.DataBindings.Add("text", bs, "ID")
+            FrageTextBox.DataBindings.Add("text", bs, "frage")
+            AntwortFreiTextBox.DataBindings.Add("text", bs, "pr_antwortFrei")
+
+            ' Multiple Choice
+            Mc_anwort1TextBox.DataBindings.Add("text", bs, "mc_anwort1")
+            Mc_check1CheckBox.DataBindings.Add("Checked", bs, "pr_mc_check1")
+
+            Mc_anwort2TextBox.DataBindings.Add("text", bs, "mc_anwort2")
+            Mc_check2CheckBox.DataBindings.Add("Checked", bs, "pr_mc_check2")
+
+            Mc_anwort3TextBox.DataBindings.Add("text", bs, "mc_anwort3")
+            Mc_check3CheckBox.DataBindings.Add("Checked", bs, "pr_mc_check3")
+
+            Mc_anwort4TextBox.DataBindings.Add("text", bs, "mc_anwort4")
+            Mc_check4CheckBox.DataBindings.Add("Checked", bs, "pr_mc_check4")
+
+            Mc_anwort5TextBox.DataBindings.Add("text", bs, "mc_anwort5")
+            Mc_check5CheckBox.DataBindings.Add("Checked", bs, "pr_mc_check5")
+
+            ' Zurückstellen der Frage
+            ZurueckstellenCheckBox.DataBindings.Add("Checked", bs, "zurueckstellen")
+
+        Catch ex As Exception
+            ' Aufruf unserer Fehlerroutine
+            If FehlerAnezige(ex) = True Then
+                MsgBox("Anwendung wird beendet")
+                Me.Close()
+            End If
+        End Try
+
 
     End Sub
 End Class
